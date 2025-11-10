463d9058aba816ae033c097452100851b8778dbb
Christian <crisdavides2004@gmail.com>
2025-11-09 23:54:04 -0500

    Arreglo de errores deploy

--- app/ecommers/login/page.tsx
+++ app/ecommers/login/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
+import React, { Suspense } from "react";
 import LoginForm from "../../../components/LoginForm";
 import "../../styles/auth.css";
-
-
 
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center auth-background">
-      <LoginForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginForm />
+      </Suspense>
     </div>
   );
 }
